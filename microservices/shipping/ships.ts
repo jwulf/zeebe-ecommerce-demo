d73579a2c86d015238cbbb16653c55aeb4b7c523
Josh Wulf <josh@magikcraft.io>
2019-07-23 00:56:04 +1000

    Update

--- microservices/shipping/ships.ts
+++ microservices/shipping/ships.ts
@@ -3,14 +3,23 @@ import { ZBClient } from "zeebe-node";
 const zb = new ZBClient("localhost");
 
 async function main() {
-  zb.createWorker("payment-worker", "ship-items", (job, complete) => {
-    const operation_success = true;
-    const { variables } = job;
-    const { product, name } = variables;
-    const outcome_message = `Shipped ${product} to ${name}`;
-    console.log(outcome_message);
-    complete.success({ operation_success, outcome_message });
-  });
+  zb.createWorker(
+    "payment-worker",
+    "ship-items",
+    (job, complete) => {
+      const operation_success = true;
+      const { variables } = job;
+      const { product, name } = variables;
+      setTimeout(() => {
+        const outcome_message = `Shipped ${product} to ${name}`;
+        console.log(outcome_message);
+        complete.success({ operation_success, outcome_message });
+      }, 2000);
+    },
+    {
+      timeout: 30000
+    }
+  );
 }
 
 main();
