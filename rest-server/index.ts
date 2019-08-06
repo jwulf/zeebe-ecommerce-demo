eb5d5254f68f7f30d847cb18cd6254e27294573e
Josh Wulf <josh@magikcraft.io>
2019-08-06 11:53:20 +1000

    Add comments, improve code structure

--- rest-server/index.ts
+++ rest-server/index.ts
@@ -2,28 +2,39 @@ import express from "express";
 import bodyParser from "body-parser";
 import { ZBClient } from "zeebe-node";
 
-const REST_TIMEOUT = 9000;
-const app = express();
-const port = 3000;
 const callbacks = new Map();
 const cachedOutcomes = new Map();
+
 const zb = new ZBClient("localhost");
 
+/** Main */
+deployWorkflow()
+  .then(startOutcomeWorker)
+  .then(startRESTServer);
+
 /** REST Server */
-app.use(bodyParser.json()); // support json encoded bodies
+async function startRESTServer() {
+  const app = express();
+  const port = 3000;
 
-app.post("/api/purchase", purchaseRouteHandler);
-app.get("/api/callback/:workflowInstanceKey", outcomeCallbackOnce);
-app.use(express.static("public"));
-
-app.listen(port, () =>
-  console.log(`\nZeebe Storefront REST API listening on port ${port}`)
-);
+  app.use(bodyParser.json());
+  app.post("/api/purchase", purchaseRouteHandler);
+  app.get("/api/callback/:workflowInstanceKey", outcomeCallbackOnce);
+  app.use(express.static("public"));
+  app.listen(port, () =>
+    console.log(`\nZeebe Storefront REST API listening on port ${port}`)
+  );
+}
 
 async function purchaseRouteHandler(
   req: express.Request,
   res: express.Response
 ) {
+  /**
+   * The client times the request out in 10 seconds. We will respond with a callback url
+   * if we don't get a workflow outcome in 9 seconds.
+   */
+  const REQUEST_TIMEOUT = 9000;
   const { product, creditcard, name } = req.body;
   console.log(`Order for ${name}: ${product} with payment: ${creditcard}`);
 
@@ -35,22 +46,30 @@ async function purchaseRouteHandler(
 
   const { workflowInstanceKey } = wfi;
 
+  /**
+   * This callback closes the request with the workflow outcome if it arrives in time
+   */
   callbacks.set(workflowInstanceKey, outcome => {
     clearTimeout(sendCallbackUrl);
     res.json(outcome);
   });
 
-  // Avoid client timeout when workflow is stalled
-  // by sending a callback url that the client can poll
+  /**
+   * If the workflow does not complete in time, send back a callback url that the client can poll
+   */
   const sendCallbackUrl = setTimeout(() => {
     callbacks.delete(workflowInstanceKey);
     console.log(`Workflow timed out for ${name}. Sending async callback URL`);
     res.json({
       callback: `/api/callback/${workflowInstanceKey}`
     });
-  }, REST_TIMEOUT);
+  }, REQUEST_TIMEOUT);
 }
 
+/**
+ * Callback route handler to allow clients to poll for a long-running workflow outcome.
+ * Returns the workflow outcome if it is available, 404 if not.
+ */
 function outcomeCallbackOnce(req, res) {
   const { workflowInstanceKey } = req.params;
   if (cachedOutcomes.has(workflowInstanceKey)) {
@@ -62,20 +81,23 @@ function outcomeCallbackOnce(req, res) {
   }
 }
 
-/** Deploy Workflow */
+/** Deploy Workflow to Zeebe when REST Server starts */
 async function deployWorkflow() {
   const wf = await zb.deployWorkflow("./bpmn/purchase.bpmn");
   console.log("\nDeployed Workflow:");
   console.log(wf);
 }
 
-deployWorkflow().then(startOutcomeWorker);
-
 /** ZB Outcome Worker */
-function startOutcomeWorker() {
+async function startOutcomeWorker() {
   zb.createWorker("outcome-worker", "publish-outcome", (job, complete) => {
     const { workflowInstanceKey, variables } = job;
     const { operation_success, outcome_message } = variables;
+
+    /**
+     * Put this in a try-finally block to ensure the job completes even if the response callback
+     * fails (due to closed connection, etc...)
+     * */
     try {
       const isRestRequestStillAlive = callbacks.has(workflowInstanceKey);
       if (isRestRequestStillAlive) {
